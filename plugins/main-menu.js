716b59f8e5f491a97d88df235ca46ce6e1afb4b9
SIRIUS-CYBER <herokutruem@gmail.com>
2025-05-29 17:28:07 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -10,7 +10,7 @@ cmd({
     use: '.menu',
     desc: "Show all bot commands",
     category: "menu",
-    react: "📂",
+    react: "♾️",
     filename: __filename
 }, 
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
