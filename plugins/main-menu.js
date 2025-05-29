5a2220587bb9e74554b01be7b59cf2c6a6dbd3ce
SIRIUS-CYBER <herokutruem@gmail.com>
2025-05-29 16:08:31 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,9 +1,17 @@
-const config = require('../config')
-const { cmd, commands } = require('../command');
+const config = require('../config');
 const moment = require('moment-timezone');
-const os = require("os")
-const {runtime} = require('../lib/functions')
-const axios = require('axios')
+const { cmd, commands } = require('../command');
+const axios = require('axios');
+
+function toSmallCaps(str) {
+  const smallCaps = {
+    A: 'ᴀ', B: 'ʙ', C: 'ᴄ', D: 'ᴅ', E: 'ᴇ', F: 'ғ', G: 'ɢ', H: 'ʜ',
+    I: 'ɪ', J: 'ᴊ', K: 'ᴋ', L: 'ʟ', M: 'ᴍ', N: 'ɴ', O: 'ᴏ', P: 'ᴘ',
+    Q: 'ǫ', R: 'ʀ', S: 's', T: 'ᴛ', U: 'ᴜ', V: 'ᴠ', W: 'ᴡ', X: 'x',
+    Y: 'ʏ', Z: 'ᴢ'
+  };
+  return str.toUpperCase().split('').map(c => smallCaps[c] || c).join('');
+}
 
 cmd({
     pattern: "menu",
